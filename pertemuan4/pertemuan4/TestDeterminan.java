81e0400beb47c544feb26b7bb3f224fca5d2afd3
people pineapple <113289837+Nilapine@users.noreply.github.com>
2024-04-16 13:30:47 +0700

    Delete pertemuan4/pertemuan4 directory

--- pertemuan4/pertemuan4/TestDeterminan.java
+++
@@ -1,9 +0,0 @@
-7public class TestDeterminan{
-	public static void main(String[] a) {
-		Determinan dtm=new Determinan();
-		dtm.inputData();
-		dtm.hitungD();
-		dtm.hitungX();
-		dtm.cetak();
-	}
-}
