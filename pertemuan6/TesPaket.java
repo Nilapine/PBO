8137157ec323d5aa522481d5c3ee265f466213d0
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-05-04 17:47:03 +0700

    pertemuan6

---
+++ pertemuan6/TesPaket.java
@@ -0,0 +1,11 @@
+import kelasku.Joko;
+import kelasku.Budi;
+
+public class TesPaket {
+    public static void main(String[] args) {
+        Budi ObjectBudi=new Budi();
+        ObjectBudi.info();
+        Joko objectJoko=new Joko();
+        objectJoko.info();
+    }
+}
