789ee4fdbc58d948e283f10afb7eb4fa8492f97a
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-06-02 07:11:46 +0700

    done

---
+++ pertemuan1/persegi.java
@@ -0,0 +1,14 @@
+public class persegi {
+public static void main (String args[])
+{
+    int s;
+    s = 20;
+    int luas=s*s;
+    int kel=s+s+s+s;
+
+    System.out.println("Sisi : "+s);
+    System.out.println("Luas = "+luas);
+    System.out.println("Keliling = "+kel);
+}
+    
+}
